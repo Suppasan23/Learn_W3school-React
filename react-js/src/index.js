07f1eabd740ce4c79f1b466a244dacf43651ce85
Suppasan23 <suppasan.benz@gmail.com>
2023-03-16 09:44:19 +0000

    4:45pm 16/03/2023

--- react-js/src/index.js
+++ react-js/src/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Football from './Football';
-
-ReactDOM.render(<Football/>, document.getElementById('root1'));
+import ReactDOM from 'react-dom/client';
 
 
+const cars = 
+[
+  {id: 1, brand: 'Ford'},
+  {id: 2, brand: 'BMW'},
+  {id: 3, brand: 'Audi'}
+];
+
+
+const root = ReactDOM.createRoot(document.getElementById('root1'));
+root.render(<Garage cars={cars}/>);
+
+
+function Garage(props)
+{
+
+  return(
+    <div>
+      <h1>Who live in my Garage?</h1>
+      <ul>
+          {props.cars.map((car) => <Car key={car.id} brand={car.brand} carId={car.id}/>)}
+      </ul>
+    </div>
+  )
+
+}
+
+
+function Car(props)
+{
+  return <li> I am a car with id {props.carId} and brand {props.brand} </li>;
+}
